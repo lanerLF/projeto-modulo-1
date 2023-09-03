075e01a3df10ab68c94fc721d4d4b3a7b28f1796
lanerLF <135287624+lanerLF@users.noreply.github.com>
2023-09-03 19:43:54 -0300

    feat: adds Login form

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,7 +1,38 @@
 <template>
-    <div></div>
+    <v-container class="fill-height">
+        <v-row align="center" justify="center">
+            <v-col cols="3">
+                <v-card elevation="12" align="center" justify="center">
+                    <v-form class="pa-5 d-flex flex-column align-center">
+                        <v-container>
+                            <v-text-field
+                                label="Email"
+                                variant="outlined"
+                                rounded="xl"
+                            ></v-text-field>
+
+                            <v-text-field
+                                label="Password"
+                                variant="outlined"
+                                rounded="xl"
+                            ></v-text-field>
+                        </v-container>
+
+                        <v-btn
+                            append-icon="mdi-login"
+                            size="x-large"
+                            rounded="xl"
+                            elevation="4"
+                            density="compact"
+                            >Login</v-btn
+                        >
+                    </v-form>
+                </v-card>
+            </v-col>
+        </v-row>
+    </v-container>
 </template>
 <script>
 export default {};
 </script>
-<style></style>
+<style scoped></style>
