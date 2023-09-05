627c7a711cbf40e42f15518d522e24d3107e52bd
lanerLF <135287624+lanerLF@users.noreply.github.com>
2023-09-05 19:24:22 -0300

    feat: implement dashboard design and functionality

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,8 +1,32 @@
 <template>
     <v-container class="d-flex flex-row align-center justify-space-around">
         <v-container class="d-flex flex-column align-center">
-            <v-card elevation="6" class="pa-5">
-                {{ dashboard_data }}
+            <v-card elevation="6" class="pa-10 d-flex flex-column">
+                <h2>Bem vindo, {{ user_data.name }}</h2>
+                <v-container
+                    class="d-flex flex-row align-center justify-center border border-dark"
+                >
+                    <v-container
+                        class="d-flex flex-column align-start justify-center"
+                    >
+                        <h3 class="py-4">
+                            {{ dashboard_data.amount_students }} Alunos
+                        </h3>
+                        <v-btn append-icon="mdi-plus" density="compact"
+                            >Adicionar</v-btn
+                        >
+                    </v-container>
+                    <v-container
+                        class="d-flex flex-column align-start justify-center"
+                    >
+                        <h3 class="py-4">
+                            {{ dashboard_data.amount_exercises }} Exercícios
+                        </h3>
+                        <v-btn append-icon="mdi-plus" density="compact"
+                            >Adicionar</v-btn
+                        >
+                    </v-container>
+                </v-container>
             </v-card>
         </v-container>
     </v-container>
@@ -22,7 +46,7 @@ export default {
             .then((res) => (this.dashboard_data = res.data))
             .catch((error) => console.log(error));
 
-        this.user_data = JSON.parse(localStorage.getItem("user-ifo"));
+        this.user_data = JSON.parse(localStorage.getItem("user-info")) || null;
     },
 };
 </script>
