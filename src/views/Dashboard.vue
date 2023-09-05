d74dbea4cbfb55e877457532896cd0f85c9d924a
lanerLF <135287624+lanerLF@users.noreply.github.com>
2023-09-05 18:56:59 -0300

    feat: partial dashboard implementation

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,7 +1,29 @@
 <template>
-    <div></div>
+    <v-container class="d-flex flex-row align-center justify-space-around">
+        <v-container class="d-flex flex-column align-center">
+            <v-card elevation="6" class="pa-5">
+                {{ dashboard_data }}
+            </v-card>
+        </v-container>
+    </v-container>
 </template>
 <script>
-export default {};
+import axios from "axios";
+export default {
+    data() {
+        return {
+            dashboard_data: {},
+            user_data: {},
+        };
+    },
+    mounted() {
+        const axios_get = axios
+            .get("http://localhost:3000/dashboard")
+            .then((res) => (this.dashboard_data = res.data))
+            .catch((error) => console.log(error));
+
+        this.user_data = JSON.parse(localStorage.getItem("user-ifo"));
+    },
+};
 </script>
 <style scoped></style>
