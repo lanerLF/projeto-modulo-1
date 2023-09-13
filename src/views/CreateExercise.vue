b9727ac6b72a4668149a7183348fc95868cd809e
lanerLF <135287624+lanerLF@users.noreply.github.com>
2023-09-13 17:59:30 -0300

    fix: fixes typo in ref="form" in v-form

--- src/views/CreateExercise.vue
+++ src/views/CreateExercise.vue
@@ -4,7 +4,7 @@
     <v-divider class="border-opacity-75" ></v-divider>
     <div class="d-flex flex-column">
       <div class="d-flex flex-column">
-        <v-form href="form" @submit.prevent="handle_submit" class="d-flex flex-row pa-5 ma-2 align-start justify-center">
+        <v-form ref="form" @submit.prevent="handle_submit" class="d-flex flex-row pa-5 ma-2 align-start justify-center">
           <v-text-field
             label="Nome do exercício"
             placeholder="Digite o nome do exercício..."
