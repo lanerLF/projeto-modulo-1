7cf91db7aa1be9ff7fd5342231a8f131bff68be7
lanerLF <135287624+lanerLF@users.noreply.github.com>
2023-09-13 17:46:31 -0300

    fix: fixes the missing form component

--- src/views/CreateExercise.vue
+++ src/views/CreateExercise.vue
@@ -3,15 +3,17 @@
     <h2>Exercícios</h2>
     <v-divider class="border-opacity-75" ></v-divider>
     <div class="d-flex flex-column">
-      <div class="d-flex flex-row pa-6 justify-center align-start" >
-        <v-text-field
-          label="Nome do exercício"
-          placeholder="Digite o nome do exercício..."
-          variant="outlined"
-          v-model="exercise_name"
-          :rules="[value => !!value || 'O campo deve ser preenchido corretamente!']"
-          ></v-text-field>
-        <v-btn size="x-large" color="#1e90ff" class="px-2 mx-5 text-center" >Cadastrar</v-btn>
+      <div class="d-flex flex-column">
+        <v-form @submit.prevent="handle_submit" class="d-flex flex-row pa-5 ma-2 align-start justify-center">
+          <v-text-field
+            label="Nome do exercício"
+            placeholder="Digite o nome do exercício..."
+            variant="outlined"
+            v-model="exercise_name"
+            :rules="[value => !!value || 'O campo deve ser preenchido corretamente!']"
+            ></v-text-field>
+          <v-btn size="x-large" color="#1e90ff" class="px-2 mx-5 text-center" >Cadastrar</v-btn>
+        </v-form>
       </div>
     </div>
   </div>
