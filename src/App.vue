b1b2ab48daf6e3757ee2c6225457f7e0c8cef590
lanerLF <135287624+lanerLF@users.noreply.github.com>
2023-09-14 20:36:55 -0300

    fix: prettier the code

--- src/App.vue
+++ src/App.vue
@@ -3,26 +3,26 @@ import { RouterLink, RouterView } from "vue-router";
 </script>
 
 <template>
-    <v-container class="ma-5">
-        <v-layout>
-            <v-app-bar :elevation="2">
-                <h1 class="ma-5">*nome do projeto*</h1>
-                <template v-slot:append>
-                    <RouterLink to="/dashboard">Dashboard</RouterLink>
-                    <RouterLink to="/students">Alunos</RouterLink>
-                    <RouterLink to="/about">Sobre</RouterLink>
-                    <RouterLink to="/">Logout</RouterLink>
-                </template>
-            </v-app-bar>
-        </v-layout>
-    </v-container>
-    <v-container>
-        <RouterView />
-    </v-container>
+  <v-container class="ma-5">
+    <v-layout>
+      <v-app-bar :elevation="2">
+        <h1 class="ma-5">*nome do projeto*</h1>
+        <template v-slot:append>
+          <RouterLink to="/dashboard">Dashboard</RouterLink>
+          <RouterLink to="/students">Alunos</RouterLink>
+          <RouterLink to="/about">Sobre</RouterLink>
+          <RouterLink to="/">Logout</RouterLink>
+        </template>
+      </v-app-bar>
+    </v-layout>
+  </v-container>
+  <v-container>
+    <RouterView />
+  </v-container>
 </template>
 
 <style scoped>
 a {
-    margin: 10px;
+  margin: 10px;
 }
 </style>
