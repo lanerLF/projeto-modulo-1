902f5f9de536c3360ec2d50a36b6587507da6e07
lanerLF <135287624+lanerLF@users.noreply.github.com>
2023-09-15 04:05:33 -0300

    feat: implement selector with working variables

--- src/views/ManageExercise.vue
+++ src/views/ManageExercise.vue
@@ -1,23 +1,24 @@
 <template>
   <div class="ma-5">
-    <h2>Exercícios</h2>
+    <h2>Montagem de Treino</h2>
     <v-divider class="border-opacity-75"></v-divider>
     <div class="d-flex flex-column">
       <div class="d-flex flex-column">
         <v-form
           ref="form"
-          @submit.prevent="handle_submit"
           class="d-flex flex-row pa-5 ma-2 align-start justify-center"
+          @submit.prevent="handle_submit"
         >
-          <v-text-field
+          <v-select
             label="Nome do exercício"
             placeholder="Digite o nome do exercício..."
             variant="outlined"
-            v-model="exercise_name"
+            v-model="selected_item"
+            :items="exercise_list"
             :rules="[
               (value) => !!value || 'O campo deve ser preenchido corretamente!',
             ]"
-          ></v-text-field>
+          ></v-select>
           <v-btn
             type="submit"
             size="x-large"
@@ -31,21 +32,34 @@
   </div>
 </template>
 <script>
+import axios from "axios";
 export default {
   data() {
     return {
       exercise_name: "",
+      exercise_data: {},
+      exercise_list: [],
+      selected_item: null,
     };
   },
   methods: {
-    async handle_submit() {
-      const { valid } = await this.$refs.form.validate();
-
-      if (!valid) {
-        alert("PREENCHA OS CAMPOS CORRETAMENTE ANTES DE CADASTRAR!");
-        return;
-      }
+    async handle_get() {
+      const axios_get = await axios
+        .get("http://localhost:3000/exercises")
+        .then((res) => (this.exercise_data = res.data))
+        .catch((error) => alert(error));
+      console.log(this.exercise_data);
+      this.exercise_list = this.exercise_data.map((item) => {
+        console.log(item.description);
+        return item.description;
+      });
     },
+    async handle_submit() {
+      console.log(this.selected_item);
+    },
+  },
+  mounted() {
+    this.handle_get();
   },
 };
 </script>
