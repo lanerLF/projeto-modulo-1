16be33101fb7bfd9666cce46ef4dea5232eebb61
lanerLF <135287624+lanerLF@users.noreply.github.com>
2023-09-14 23:00:22 -0300

    fix: formats the file with prettier

--- src/views/CreateExercise.vue
+++ src/views/CreateExercise.vue
@@ -1,18 +1,30 @@
 <template>
-  <div class="ma-5" >
+  <div class="ma-5">
     <h2>Exercícios</h2>
-    <v-divider class="border-opacity-75" ></v-divider>
+    <v-divider class="border-opacity-75"></v-divider>
     <div class="d-flex flex-column">
       <div class="d-flex flex-column">
-        <v-form ref="form" @submit.prevent="handle_submit" class="d-flex flex-row pa-5 ma-2 align-start justify-center">
+        <v-form
+          ref="form"
+          @submit.prevent="handle_submit"
+          class="d-flex flex-row pa-5 ma-2 align-start justify-center"
+        >
           <v-text-field
             label="Nome do exercício"
             placeholder="Digite o nome do exercício..."
             variant="outlined"
             v-model="exercise_name"
-            :rules="[value => !!value || 'O campo deve ser preenchido corretamente!']"
-            ></v-text-field>
-          <v-btn type="submit" size="x-large" color="#1e90ff" class="px-2 mx-5 text-center" >Cadastrar</v-btn>
+            :rules="[
+              (value) => !!value || 'O campo deve ser preenchido corretamente!',
+            ]"
+          ></v-text-field>
+          <v-btn
+            type="submit"
+            size="x-large"
+            color="#1e90ff"
+            class="px-2 mx-5 text-center"
+            >Cadastrar</v-btn
+          >
         </v-form>
       </div>
     </div>
@@ -20,23 +32,21 @@
 </template>
 <script>
 export default {
-    data(){
-      return{
-        exercise_name: "",
+  data() {
+    return {
+      exercise_name: "",
+    };
+  },
+  methods: {
+    async handle_submit() {
+      const { valid } = await this.$refs.form.validate();
+
+      if (!valid) {
+        alert("PREENCHA OS CAMPOS CORRETAMENTE ANTES DE CADASTRAR!");
+        return;
       }
     },
-  methods: {
-   async handle_submit(){
-     const { valid } = await this.$refs.form.validate()
-
-     if (!valid) {
-       alert("PREENCHA OS CAMPOS CORRETAMENTE ANTES DE CADASTRAR!")
-       return;
-    }
-
-    }
-  }
-}
+  },
+};
 </script>
-<style scoped>
-</style>
+<style scoped></style>
