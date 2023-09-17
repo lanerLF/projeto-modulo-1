28ebfd301fd5603f0e35b96898aaae357f1945b1
lanerLF <135287624+lanerLF@users.noreply.github.com>
2023-09-17 03:00:22 -0300

    feat: basic form  styling

--- src/views/ManageExercise.vue
+++ src/views/ManageExercise.vue
@@ -1,34 +1,60 @@
-<template>
-  <div class="ma-5">
+<template style="height: 100vh; width: 100vw">
+  <div class="ma-5 mh-100 mw-100 d-flex flex-column">
     <h2>Montagem de Treino</h2>
     <v-divider class="border-opacity-75"></v-divider>
-    <div class="d-flex flex-column">
-      <div class="d-flex flex-column">
-        <v-form
-          ref="form"
-          class="d-flex flex-row pa-5 ma-2 align-start justify-center"
-          @submit.prevent="handle_submit"
-        >
-          <v-select
-            label="Nome do exercício"
-            placeholder="Digite o nome do exercício..."
-            variant="outlined"
-            v-model="selected_item"
-            :items="exercise_list"
-            :rules="[
-              (value) => !!value || 'O campo deve ser preenchido corretamente!',
-            ]"
-          ></v-select>
-          <v-btn
-            type="submit"
-            size="x-large"
-            color="#1e90ff"
-            class="px-2 mx-5 text-center"
-            >Cadastrar</v-btn
-          >
-        </v-form>
-      </div>
-    </div>
+    <v-form
+      class="d-flex flex-column align-center justify-center w-100 h-100"
+      ref="form"
+      @submit.prevent="handle_submit"
+    >
+      <v-select
+        class="mw-100 w-50"
+        label="Nome do exercício"
+        placeholder="Digite o nome do exercício..."
+        variant="outlined"
+        v-model="selected_item"
+        :items="exercise_list"
+        :rules="[
+          (value) => !!value || 'O campo deve ser preenchido corretamente!',
+        ]"
+      ></v-select>
+      <v-text-field
+        class="mw-100 w-50"
+        label="Repetições"
+        placeholder="Repetições..."
+        v-model="repetitions"
+        variant="outlined"
+        :rules="[
+          (value) => !!value || 'O campo deve ser preenchido!',
+          (value) => value >= 1 || 'O valor mínimo é uma repetição!',
+        ]"
+      ></v-text-field>
+      <v-text-field
+        class="mw-100 w-50"
+        variant="outlined"
+        label="Quilos"
+        placeholder="Quilos..."
+        v-model="kilograms"
+        :rules="[(value) => !!value || 'O campo deve ser preenchido!']"
+      ></v-text-field>
+      <v-text-field
+        class="mw-100 w-50"
+        variant="outlined"
+        label="Pausa"
+        placeholder="Pausa..."
+        v-model="pause"
+        :rules="[(value) => !!value || 'O campo deve ser preenchido!']"
+      ></v-text-field>
+      <v-text-field
+        class="mw-100 w-50"
+        variant="outlined"
+        label="Dia da semana"
+        placeholder="Dia da semana..."
+        v-model="week_day"
+        :rules="[(value) => !!value || 'O campo deve ser preenchido!']"
+      ></v-text-field>
+      <v-btn type="submit" color="#1e90ff" >Cadastrar</v-btn>
+    </v-form>
   </div>
 </template>
 <script>
@@ -40,6 +66,10 @@ export default {
       exercise_data: {},
       exercise_list: [],
       selected_item: null,
+      repetitions: 0,
+      kilograms: 0,
+      pause: "",
+      week_day: "",
     };
   },
   methods: {
